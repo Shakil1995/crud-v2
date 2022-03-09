59ae87754fd315e65828dc141a2f0b5c1475e7db
shakil1995 <na.shakil1995@gmail.com>
2022-03-09 10:27:22 +0600

    updated file

--- database/migrations/2022_03_09_040735_create_categories_table.php
+++ database/migrations/2022_03_09_040735_create_categories_table.php
@@ -6,17 +6,25 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateCategoriesTable extends Migration
 {
-    
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->nullable();
+            $table->string('category_name')->nullable();
             $table->timestamps();
         });
     }
 
-  
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
     public function down()
     {
         Schema::dropIfExists('categories');
